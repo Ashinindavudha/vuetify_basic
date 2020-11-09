d1e08021bf76382ddda51fe8b9a5ed4aa05431fd
AshinIndavudha <ashinindavudha2018@gmail.com>
2020-11-09 09:30:17 +0630

    First_init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+ <nav>
+  
+  <v-app-bar flat app-bar color="deep-purple accent-2" dark>
+   
+   <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+   
+  <v-toolbar-title class="text-uppercase dark--text">
+   <span class="font-weight-light">
+    ToDo
+   </span>
+   <span>Dhammavira</span>
+  </v-toolbar-title>
+  <v-spacer></v-spacer>
+  <v-btn text color="dark">
+   <span class="text-uppercase dark--text">Sign Out</span>
+   <v-icon right>mdi-login</v-icon>
+  </v-btn>
+  </v-app-bar>
+  <v-navigation-drawer v-model="drawer" app class="blue lighten-2">
+   <p>test</p>
+  </v-navigation-drawer>
+ </nav>
+</template>
+
+<script>
+ export default {
+  data() {
+   return {
+    drawer: false,
+     
+   }
+  },
+   
+ }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--  -->
+    <h1>Home Page</h1>
+    <!-- <p class="red white--text">Lorem ipsum, dolor</p>
+    <p class="lighten-4 pink red--text text--dark-4">Lorem ipsum dolor</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h1 class="display-1">Smaller Display</h1>
+    <p class="headline">This is a HeadLine</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+    <p class="caption">Caption</p> -->
+    <!-- <v-btn class="pink white--text">Click Me</v-btn>
+    <v-btn dark color="pink">Click Me</v-btn>
+    <v-btn depressed color="pink">Click Me</v-btn>
+    <v-btn text color="pink">Click Me</v-btn>
+    <v-btn depressed class="pink white--text"><v-icon
+      large color="green darken-2">
+      mdi-email
+    </v-icon>
+    </v-btn>
+    <v-btn
+        class="ma-2"
+        color="primary"
+        dark
+      >
+        Accept
+        <v-icon
+          dark
+          right
+        >
+          mdi-checkbox-marked-circle
+        </v-icon>
+      </v-btn> -->
+      <!-- Break Point -->
+      <!-- <v-btn class="hidden-md-and-down">Cick Me</v-btn> -->
+       <!-- <v-btn class="hidden-md-and-up">Cick Me</v-btn> -->
+        <!-- <v-btn class="hidden-sm-only">Cick Me</v-btn> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
